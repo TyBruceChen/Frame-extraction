dc001ed43eea65d55cb644ad70359afabb056b0b
Yongjie Fu <yf2578@columbia.edu>
2025-04-17 19:37:07 -0400

    'modify'

---
+++ README.md
@@ -0,0 +1,12 @@
+# Frame Extraction
+
+This project processes a video and extracts frames to the `cycle_vid/` folder.
+
+## Instructions
+
+```
+1. git clone the repo.
+2. Installed the required package.
+3. change the name of the video to process. (Lane: 30)
+4. Running the code, get the images in the folder cycle_vid/
+```
--- video_labeler_2.py
+++ video_labeler_2.py
@@ -1,8 +1,5 @@
 import os
-from sahi.utils.yolov8 import download_yolov8s_model
 from sahi import AutoDetectionModel
-from sahi.utils.cv import read_image
-from sahi.utils.file import download_from_url
 from sahi.predict import get_prediction, get_sliced_prediction, predict
 from pathlib import Path
 from IPython.display import Image
